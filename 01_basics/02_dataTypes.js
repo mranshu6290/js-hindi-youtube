7c5294864593053eb97e4ffdc8e924da06f1456a
mranshu6290 <mranshu6290@gmail.com>
2024-10-17 09:41:23 +0000

    datatypes explained

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all files for new versions
+alert("Hello"); // This alert works only in web browsers, not in Node.js
+
+// Standards are written by ECMA (TC39)
+// Refer to MDN for documentation created by Mozilla
+
+/*
+Primitive Data Types:
+- Number
+- BigInt
+- String
+- Boolean
+- null: A type and standalone value representing empty
+  Example: For faulty temperature sensors, return null instead of 0.
+- undefined: Indicates a value has not been assigned, also a type
+*/
+
+/**
+ * typeof: Tells us the type
+ * typeof null is "object"
+ * typeof undefined is "undefined"
+ */
