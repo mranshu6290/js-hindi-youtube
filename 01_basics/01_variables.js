d639910c4022636ada96c4ddd0911069b04fc78c
mranshu6290 <mranshu6290@gmail.com>
2024-10-17 09:24:27 +0000

    added Variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+// Defining Variables
+// 'const' for constants, 'let' for reassigned variables, avoid 'var' due to scope issues.
+/**
+ * Example of defining a variable:
+ * let accountName = "Jaipur"; // Declared with 'let'
+ * let userState; // Uninitialized 'let' variable
+ * console.log(userState); // Output: 'undefined'
+ * Using console.table to display multiple values:
+ * const a = 1, b = 2, c = 3, d = 4;
+ * console.table([a, b, c, d]); // Displays values in a table format
+ */
+
+
+const accountId =1234
+
+accountId =234
+
+console.log(accountId);
+
+console.table
