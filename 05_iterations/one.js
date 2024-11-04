b1614eef980f362a3ebaa1adfb521742ca98818a
mranshu6290 <mranshu6290@gmail.com>
2024-11-04 07:33:45 +0000

    one

---
+++ 05_iterations/one.js
@@ -0,0 +1,45 @@
+// for loop
+
+/**
+ * 
+ * 
+ * for (let i=0;i<arr.length;i++)
+ * {
+ * const element = array[i]
+ * }
+ * 
+ */
+
+
+/**
+ * For of 
+ * 
+ * Iterator of object
+ * 
+ * For Of loop
+ * 
+ * for(const num of object/arr){}  this does not ned end and increment
+ * 
+ * this can be done on string as well
+ * 
+ * 
+ * 
+ */
+
+
+/**
+ * Maps
+ * 
+ * Map is an object
+ * 
+ * const map =new Map()
+ * 
+ * Holds key value of keys in insertion order, no duplicate keys
+ * 
+ * map.set('IN', "India")
+ *
+ *
+ * Looping in map
+ * 
+ *  
+ */
